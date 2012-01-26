da059152a34e658395d65a13df96e416e130cfa2
Alex H Frey <alex.h.frey@gmail.com>
2012-01-26 15:28:54 -0800

    Added line for heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook, '333924066629604', '0cc1164e4805256031da55dbb9740125' 
+	{:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+
 	
 end
 
