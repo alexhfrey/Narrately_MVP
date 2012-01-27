243dc527b89a60a6f79394ba0d9d5fada2e207a3
alexhfrey <alex.h.frey@gmail.com>
2012-01-27 12:28:46 -0800

    Removed file location to test Heroku ImageMagick

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
 belongs_to :user
-has_attached_file :output_file, :path => ":rails_root/public/User_Uploads"
-has_attached_file :project_image, :path => ":rails_root/public/images" , :styles => { :medium => "300x300>"  }
+has_attached_file :output_file
+has_attached_file :project_image :styles => { :medium => "300x300>"  }
 validates_attachment_content_type :output_file, :content_type=>['application/pdf'], :message => "File must be in PDF format"
 validates_attachment_content_type :project_image, :content_type=>['image/jpeg'], :message => "Cover image must be in JPEG format"
 validates_attachment_size :project_image, :less_than=> 10.megabytes
