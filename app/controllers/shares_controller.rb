f66e986ae1b8492b040a08c524ebc669895a17e0
alexhfrey <alex.h.frey@gmail.com>
2012-01-29 21:05:05 -0800

    Finished shares w/ smart forwarding and databse creation

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,11 +1,12 @@
 class SharesController < ApplicationController
   def new
   @project = Project.find(params[:project_id])
+  
   if current_user.nil?
 	session[:redirect] = params[:project_id]
 	redirect_to signin_path 
-  else
   end
+  @user = current_user
   share_page = "http://www.narrately.com/projects/" + params[:project_id]
   tweet_text = "Check out the great new project I found on @Narrately"
   via = "Narrately"
@@ -14,9 +15,9 @@ class SharesController < ApplicationController
 
   def create
  
-   @user = current_user
+   
   
-  @share = Shares.new[:user_id => @user.id, :project_id => params[:project_id]]
+  @share = Shares.new(:user_id => params[:user_id], :project_id => params[:project_id])
   
   if @share.save 
 	redirect_to current_user
