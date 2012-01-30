3f1875cdadd85a969c5d508c4c35cc4fcd88ea19
alexhfrey <alex.h.frey@gmail.com>
2012-01-30 14:18:51 -0800

    css / styling for project page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,15 +10,19 @@ class ProjectsController < ApplicationController
 	@project = @user.projects.build(params[:project])
 	
 	if @project.save
+		flash[:success] = "Thanks for your submission!"
 		render :confirmation
-	else 	
-	render 'new'
+	else 
+		flash[:error] = "A few minor issues..."
+		render 'new'
 	end
   end
 
   def show
   @project = Project.find(params[:id])
-  @user = User.find(@project.user_id)
+  @promotion_clear = Shares.find_all_by_project_id(@project.id).length < @project.promotion_limit && 
+					Shares.find_by_user_id_and_project_id(current_user.id, @project.id).nil?
+  
   end
   
   def confirmation
