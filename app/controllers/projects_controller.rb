60ddc4887e3ad67bc35af561566040f01b28cad7
Alex H Frey <alex.h.frey@gmail.com>
2012-01-30 18:50:17 -0800

    erik changes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,17 +19,23 @@ class ProjectsController < ApplicationController
   end
 
   def show
-   @project = Project.find(params[:id])
-   @user = creator = @project.user
+  @project = Project.find(params[:id])
+  @creator = @project.user
+  # for testing purposes
   if @project.promotion_limit.nil?
 	@project.promotion_limit = 10
   end
- 
-  @promotion_clear = Shares.find_all_by_project_id(@project.id).length < @project.promotion_limit && 
-					Shares.find_by_user_id_and_project_id(current_user.id, @project.id).nil?
+  ###
+   
   
+  @promotions_left = @project.promotion_limit - Shares.find_all_by_project_id(@project.id).length
+  @promotions_clear = ( @promotions_left > 0 )
+  
+  if current_user 
+	@promotions_clear = (@promotions_clear && Shares.find_by_user_id_and_project_id(current_user.id, @project.id).nil?  )
+	else
   end
-  
+ end
   def confirmation
   end
 
