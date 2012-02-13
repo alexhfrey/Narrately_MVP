ecb312e860037c0457ff08ee327af9e01ade2f0c
alexhfrey <alex.h.frey@gmail.com>
2012-02-12 22:33:42 -0800

    fb credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,7 +7,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	end
 else
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, '242735669136491', 'ea405d01fda59ee513e230cf3a779d0f', :scope => 'email, offline_access'  
+	provider :facebook, '173481316095568', 'a106992ae684af007857e95876a8172a', :scope => 'email, offline_access'  
 	provider :twitter, '1NaBaXI8sNsnACN0AfzNhg', '4CSeBQJdTRf5qsY37wNmglMriSifAROovaCwb9ENXDk'
 	provider :identity
   
