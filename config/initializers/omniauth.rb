664ac0efe24db6048a2a75bda037ea26c94d32e0
alexhfrey <alex.h.frey@gmail.com>
2012-02-12 22:35:27 -0800

    updated credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,14 @@
 if Rails.env.production?  
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, '333924066629604', '0cc1164e4805256031da55dbb9740125' 	
+	provider :facebook, '173481316095568', 'a106992ae684af007857e95876a8172a', :scope => 'email, offline_access' 	
+	provider :twitter, '1NaBaXI8sNsnACN0AfzNhg', '4CSeBQJdTRf5qsY37wNmglMriSifAROovaCwb9ENXDk'
 	provider :identity
   
 	{:client_options => {:ssl => {:ca_path => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 	end
 else
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, '173481316095568', 'a106992ae684af007857e95876a8172a', :scope => 'email, offline_access'  
+	provider :facebook, '242735669136491', 'ea405d01fda59ee513e230cf3a779d0f', :scope => 'email, offline_access'  
 	provider :twitter, '1NaBaXI8sNsnACN0AfzNhg', '4CSeBQJdTRf5qsY37wNmglMriSifAROovaCwb9ENXDk'
 	provider :identity
   
