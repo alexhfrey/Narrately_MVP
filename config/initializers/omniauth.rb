8086433f515675ae3b140114434cf48e487bab1b
Alex H Frey <alex.h.frey@gmail.com>
2012-02-20 13:54:06 -0800

    changed twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,16 +1,16 @@
 if Rails.env.production?  
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook, '173481316095568', 'a106992ae684af007857e95876a8172a', :scope => 'email, offline_access' 	
-	provider :twitter, '1NaBaXI8sNsnACN0AfzNhg', '4CSeBQJdTRf5qsY37wNmglMriSifAROovaCwb9ENXDk'
-	provider :identity
+	provider :twitter, 'aIWATKSZGOQlGcFyFu9yhw', 'mKLlGiCnI6jSE8tHP8xbJgqx8ITXwdCAetuvgYM'
+	
   
 	{:client_options => {:ssl => {:ca_path => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 	end
 else
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook, '242735669136491', 'ea405d01fda59ee513e230cf3a779d0f', :scope => 'email, offline_access'  
-	provider :twitter, '1NaBaXI8sNsnACN0AfzNhg', '4CSeBQJdTRf5qsY37wNmglMriSifAROovaCwb9ENXDk'
-	provider :identity
+	provider :twitter, 'aIWATKSZGOQlGcFyFu9yhw', 'mKLlGiCnI6jSE8tHP8xbJgqx8ITXwdCAetuvgYM'
+	
   
 	end
 end
