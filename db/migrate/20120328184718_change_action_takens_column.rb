f6ab34c3b96d7b243fec088ae752c362f12fcd31
alexhfrey <alex.h.frey@gmail.com>
2012-03-28 11:54:55 -0700

    bug

--- db/migrate/20120328184718_change_action_takens_column.rb
+++ db/migrate/20120328184718_change_action_takens_column.rb
@@ -1,5 +1,5 @@
 class ChangeActionTakensColumn < ActiveRecord::Migration
-   change_table(:action_pages) do |t|
+   change_table(:action_takens) do |t|
 	t.remove :action_page_id
 	t.integer :action_page_id
 	end
