829e6e1088f088817c54eecf67c428e50bc858c9
alexhfrey <alex.h.frey@gmail.com>
2012-04-04 11:24:57 -0700

    facebook posts

--- app/controllers/facebook_post_controller.rb
+++ app/controllers/facebook_post_controller.rb
@@ -6,8 +6,8 @@ def new
 		if params[:post_id]
 			@shr = Share.new(:user_id => @user.id, :project_id => @project.id, :medium => "facebook", :share_id => params[:post_id], :referral => "fb_" + params[:post_id])
 			@shr.save	
-			 flash[:success] = "Success! You are now a fan club member. Show your support for" + @project.brand_name + "by taking an action below"
-			redirect_to @share.project and return 
+			 flash[:success] = "Success! You are now a fan club member. Show your support for " + @project.brand_name + "by taking an action below"
+			redirect_to @project and return 
 		end
 	redirect_to @user and return 
 end
