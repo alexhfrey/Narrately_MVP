0c8896f69f77a5c69c8855c44fcaf404de943d2b
alexhfrey <alex.h.frey@gmail.com>
2012-04-05 00:36:30 -0700

    lsjk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,16 @@
 MVP1::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  
+  config.action_mailer.default_url_options = { :host => 'mysnowball.com' }
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com'
+}
+  
  config.middleware.use ExceptionNotifier,
   :email_prefix => "[Notification] ",
   :sender_address => %{"notifier" <notifier@mysnowball.com>},
